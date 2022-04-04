69a4bcb14121ccbb99a87e7b09716b446e6b43e3
adityasimant <adityasimant88@gmail.com>
2022-04-04 17:40:27 +0530

    commit

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default  function Navbar() {
+    return (
+        <div className="mainNavbar">
+            <div className="navbar">
+                <img src="https://imgs.search.brave.com/iTkSkLya-BI-qM4jIMGT9jugqChKOTT1I73H5bJsu4c/rs:fit:1528:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5F/VlpyRS12al9obFJO/ODRyMFlCS2FRSGFD/VCZwaWQ9QXBp" alt="airbnb" width="82" height="25.35" />
+            </div>
+        </div>
+    )
+}
